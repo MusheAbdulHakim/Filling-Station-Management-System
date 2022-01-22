89638b0b1c4527e442da82c90c291178afff5ef7
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-22 11:47:03 +0000

    expense controller

---
+++ app/Http/Controllers/Admin/ExpenseCrudController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\ExpenseRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class ExpenseCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class ExpenseCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Expense::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/expense');
+        CRUD::setEntityNameStrings('expense', 'expenses');
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+
+        /**
+         * Columns can be defined using the fluent syntax or array syntax:
+         * - CRUD::column('price')->type('number');
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         */
+        CRUD::column('name');
+        CRUD::column('expense_category_id')
+            ->type('select')
+            ->entity('expenseCategory')
+            ->attribute('name')
+            ->model("app\Models\ExpenseCategory")
+            ->wrapper([
+                'href' => function ($crud, $column, $entry, $related_key) {
+                    return backpack_url('unit/'.$related_key.'/show');
+                },
+            ]);
+        CRUD::column('cost');
+        CRUD::column('comment');
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(ExpenseRequest::class);
+
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         */
+        CRUD::addField([  
+            'name'  => 'name',
+            'label' => 'Name',
+            'type'  => 'text',
+        ]);
+        CRUD::addField([  // Select2
+            'label'     => 'Category',
+            'type'      => 'select2',
+            'model' =>  'app\Models\ExpenseCategory',
+            'name'      => 'expense_category_id', // the db column for the foreign key
+            'entity'    => 'expenseCategory', // the method that defines the relationship in your Model
+            'attribute' => 'name', // foreign key attribute that is shown to user
+        ]);
+        CRUD::addField([  
+            'name'  => 'cost',
+            'label' => 'Cost',
+            'type'  => 'number',
+        ]);
+        CRUD::addField([  
+            'name'  => 'comment',
+            'label' => 'Comment',
+            'type'  => 'textarea',
+            
+        ]);
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
