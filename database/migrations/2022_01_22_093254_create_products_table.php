81a78e50b028f36eece27c14cda6750ed973ed8b
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-22 10:45:36 +0000

    products table

---
+++ database/migrations/2022_01_22_093254_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('unit_id')->nullable()->constrained()->onDelete('cascade');
+            $table->decimal('cost_price')->nullable();
+            $table->decimal('sale_price');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
