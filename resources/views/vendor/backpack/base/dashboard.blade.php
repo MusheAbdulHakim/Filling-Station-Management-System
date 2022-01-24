9a1a72ac1222a651d52c80fe75709a9566c698ce
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-24 01:59:39 +0000

    added purchase chart

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -25,49 +25,8 @@
 	// 
 	// But you do whatever you think it's best. Who am I, your mom?
 	$userCount = App\User::count();
-	
+	$purchaseCount = App\Models\Purchase::count();
  	
-
-    $widgets['after_content'][] = [
-	  'type' => 'div',
-	  'class' => 'row',
-	  'content' => [ // widgets 
-	       [
-			  'type' => 'card',
-			  // 'wrapperClass' => 'col-sm-6 col-md-4', // optional
-			  // 'class' => 'card bg-dark text-white', // optional
-			  'content' => [
-			      'header' => 'Some card title', // optional
-			      'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-			  ]
-			],
-			[
-			  'type' => 'card',
-			  // 'wrapperClass' => 'col-sm-6 col-md-4', // optional
-			  // 'class' => 'card bg-dark text-white', // optional
-			  'content' => [
-			      'header' => 'Another card title', // optional
-			      'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-			  ]
-			],
-			[
-			  'type' => 'card',
-			  // 'wrapperClass' => 'col-sm-6 col-md-4', // optional
-			  // 'class' => 'card bg-dark text-white', // optional
-			  'content' => [
-			      'header' => 'Yet another card title', // optional
-			      'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-			  ]
-			],
-	  ]
-	];
-    $widgets['after_content'][] = [
-	  'type'         => 'alert',
-	  'class'        => 'alert alert-warning bg-dark border-0 mb-4',
-	  'heading'      => 'Demo Refreshes Every Hour on the Hour',
-	  'content'      => 'At hh:00, all custom entries are deleted, all files, everything. This cleanup is necessary because developers like to joke with their test entries, and mess with stuff. But you know that :-) Go ahead - make a developer smile.' ,
-	  'close_button' => true, // show close button or not
-	];
 
     $widgets['before_content'][] = [
 	  'type' => 'div',
@@ -88,9 +47,9 @@
 		        'type' => 'chart',
 		        'wrapperClass' => 'col-md-6',
 		        // 'class' => 'col-md-6',
-		        'controller' => \App\Http\Controllers\Admin\Charts\NewEntriesChartController::class,
+		        'controller' => \App\Http\Controllers\Admin\Charts\LatestPurchaseChartController::class,
 				'content' => [
-				    'header' => 'New Entries', // optional
+				    'header' => 'Past 7 Days Purchases', // optional
 				    // 'body' => 'This chart should make it obvious how many new users have signed up in the past 7 days.<br><br>', // optional
 		    	]
 	    	],
