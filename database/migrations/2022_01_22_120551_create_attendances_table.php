5f29d3e36bc9c2109e7d01676aff69bd36fddb57
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-24 02:01:01 +0000

    attendaces table

---
+++ database/migrations/2022_01_22_120551_create_attendances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('checkin')->nullable();
+            $table->string('checkout')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
