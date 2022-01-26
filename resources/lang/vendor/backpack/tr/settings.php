8f9dafe77311b8463bb42b594455d97cf7a1124d
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-26 11:16:03 +0000

    added tr lang

---
+++ resources/lang/vendor/backpack/tr/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Settings Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Laravel Backpack - Settings
+    |
+    */
+    'name'             => 'Adı',
+    'value'            => 'Değer',
+    'description'      => 'Açıklama',
+    'setting_singular' => 'ayar',
+    'setting_plural'   => 'ayarlar',
+
+];
