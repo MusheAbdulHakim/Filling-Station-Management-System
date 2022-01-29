3e21a7e89ad23651aa00f055143f87bd365bbfcc
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-29 23:21:36 +0000

    checkin table

---
+++ database/migrations/2022_01_29_224642_create_check_ins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckInsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('check_ins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->nullable()->constrained()->onDelete('cascade');
+            $table->time('time');
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('check_ins');
+    }
+}
