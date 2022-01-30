0801c3501594b5d5a21086a01bdb30c6d0322be7
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-01-30 23:49:31 +0000

    latest sales chart added

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -4,15 +4,7 @@
 	// ---------------------
 	// JUMBOTRON widget demo
 	// ---------------------
-	// Widget::add([
- //        'type'        => 'jumbotron',
- //        'name' 		  => 'jumbotron',
- //        'wrapperClass'=> 'shadow-xs',
- //        'heading'     => trans('backpack::base.welcome'),
- //        'content'     => trans('backpack::base.use_sidebar'),
- //        'button_link' => backpack_url('logout'),
- //        'button_text' => trans('backpack::base.logout'),
- //    ])->to('before_content')->makeFirst();
+	
 
 	// -------------------------
 	// FLUENT SYNTAX for widgets
@@ -26,7 +18,49 @@
 	// But you do whatever you think it's best. Who am I, your mom?
 	$userCount = App\User::count();
 	$purchaseCount = App\Models\Purchase::count();
- 	
+ 	$salesCount = App\Models\Sale::count();
+	$supplierCount = App\Models\Supplier::count();
+	$productCount = App\Models\Product::count();
+
+	 Widget::add()->to('before_content')->type('div')->class('row')->content([
+		// notice we use Widget::make() to add widgets as content (not in a group)
+		Widget::make()
+			->type('progress')
+			->class('card border-0 text-white bg-primary')
+			->progressClass('progress-bar')
+			->value($userCount)
+			->description('Registered users.')
+			->progress(100*(int)$userCount/1000),
+		// alternatively, to use widgets as content, we can use the same add() method,
+		// but we need to use onlyHere() or remove() at the end
+		Widget::add()
+		    ->type('progress')
+		    ->class('card border-0 text-white bg-success')
+		    ->progressClass('progress-bar')
+		    ->value($purchaseCount)
+		    ->description('Purchases.')
+		    ->progress(80)
+		    ->onlyHere(), 
+		// alternatively, you can just push the widget to a "hidden" group
+		Widget::make()
+			->group('hidden')
+		    ->type('progress')
+		    ->class('card border-0 text-white bg-warning')
+		    ->value($supplierCount)
+		    ->progressClass('progress-bar')
+		    ->description('Suppliers.')
+		    ->progress(30),
+		
+		Widget::make()
+			->group('hidden')
+		    ->type('progress')
+		    ->class('card border-0 text-white bg-dark')
+		    ->value($productCount)
+		    ->progressClass('progress-bar')
+		    ->description('Products.')
+		    ->progress(90),
+		
+	]);
 
     $widgets['before_content'][] = [
 	  'type' => 'div',
@@ -50,6 +84,22 @@
 		        'controller' => \App\Http\Controllers\Admin\Charts\LatestPurchaseChartController::class,
 				'content' => [
 				    'header' => 'Past 7 Days Purchases', // optional
+				    // 'body' => 'This chart should make it obvious how many new users have signed up in the past 7 days.<br><br>', // optional
+		    	]
+	    	],
+    	]
+	];
+	$widgets['before_content'][] = [
+	  'type' => 'div',
+	  'class' => 'row',
+	  'content' => [ // widgets 
+	    	[ 
+		        'type' => 'chart',
+		        'wrapperClass' => 'col-md-6',
+		        // 'class' => 'col-md-6',
+		        'controller' => \App\Http\Controllers\Admin\Charts\LatestSalesChartController::class,
+				'content' => [
+				    'header' => 'Sales for the past 7 days', // optional
 				    // 'body' => 'This chart should make it obvious how many new users have signed up in the past 7 days.<br><br>', // optional
 		    	]
 	    	],
